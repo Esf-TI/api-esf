f087cba37b7009427748218dd6c913dd1a1e18be
diogorennam <diogorenan006@gmail.com>
2023-11-20 16:52:52 -0300

    feature: Configuração inicial do backend

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Configuração dos middlewares
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(cors({
+    origin: '*'
+}));
+
+//IMPORTAÇÃO DAS ROTAS
+const NucleosRoutes = require('../backend/routes/NucleosRoutes');
+const ProjectsRoutes = require('../backend/routes/ProjectsRoutes');
+
+app.use('/nucleos', NucleosRoutes);
+app.use('/projects', ProjectsRoutes);
+
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
